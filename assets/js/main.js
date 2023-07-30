d0b4c551d93ea1db21a1edf66eed6c0d8c765f22
UrayaBlakkat <urayablakkat@gmail.com>
2023-07-30 20:26:13 +0300

    Create open/close skills feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,22 @@ const navLink = document.querySelectorAll('.nav__link');
 navLink.forEach(link => link.addEventListener('click', () => {
 	navMenu.classList.remove('show-menu');
 }));
+
+
+// ===============> Accordion skills <===============
+const skillsContent = document.getElementsByClassName('skills__content'),
+			skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+	let itemClass = this.parentNode.className;
+
+	for (let i = 0; i < skillsContent.length; i += 1) {
+		skillsContent[i].className = 'skills__content skills__close';
+	}
+
+	if (itemClass === 'skills__content skills__close') {
+		this.parentNode.className = 'skills__content skills__open';
+	}
+}
+
+skillsHeader.forEach(el => el.addEventListener('click', toggleSkills));
