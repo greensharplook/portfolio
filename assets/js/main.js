49e8d2e1f5201f6c895f8c6e708a7221fc402bd7
UrayaBlakkat <urayablakkat@gmail.com>
2023-08-05 21:01:35 +0300

    Style the testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,7 @@ modalCloses.forEach((modalClose) => {
 
 
 // =============> Portfolio swiper <=================
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
 	cssMode: true,
 	loop: true,
 	navigation: {
@@ -100,5 +100,24 @@ let swiper = new Swiper('.portfolio__container', {
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true
+	}
+});
+
+
+// =============> Testimonial swiper <===============
+let swiperTestimonial = new Swiper('.testimonial__container', {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 48,
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true
+	},
+	breakpoints: {
+		568: {
+			slidesPerView: 2
+		}
 	}
 });
