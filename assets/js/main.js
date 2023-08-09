41a68469573215675c6301e8b939b6a2eb4ddea7
UrayaBlakkat <urayablakkat@gmail.com>
2023-08-09 17:01:08 +0300

    Create change background header feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,14 @@ function scrollActive(){
 }
 
 window.addEventListener('scroll', scrollActive);
+
+
+// =============> Change header <====================
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    
+    if(this.scrollY >= 80) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
